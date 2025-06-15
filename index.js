309c98afb0d6f789c7dbacc085aa0156d3332331
mayouspublishing <admin@mayous.org>
2025-06-15 09:17:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request))
-})
+  event.respondWith(handleRequest(event.request));
+});
 
 async function handleRequest(request) {
-  const targetSitemap = "https://mayouspublishing.blogspot.com/sitemap.xml"
-  const response = await fetch(targetSitemap)
+  const targetSitemap = "https://blog.mayous.org/sitemap.xml"; // new source
+  const response = await fetch(targetSitemap);
 
   if (!response.ok) {
-    return new Response("Failed to fetch Blogger sitemap", { status: 502 })
+    return new Response("Failed to fetch Blogger sitemap", { status: 502 });
   }
 
-  let xml = await response.text()
+  let xml = await response.text();
 
-  // Replace blogspot URLs with your domain and /blog prefix
+  // Replace blog.mayous.org links with proxied mayous.org/blog URLs
   xml = xml.replace(
-    /https:\/\/mayouspublishing\.blogspot\.com/g,
+    /https:\/\/blog\.mayous\.org/g,
     "https://www.mayous.org/blog"
-  )
+  );
 
   return new Response(xml, {
     headers: { "Content-Type": "application/xml" },
     status: 200
-  })
+  });
 }
